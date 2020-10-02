188f02105580d819a3f6e0ea70fb1b24d7e86bdd
damien <damien.villeneuve@edu.univ-fcomte.fr>
2020-10-02 15:00:01 +0200

    font tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
     enabled: false,
   },
   theme: {
+    fontFamily: {
+      'sans': ['-apple-system', 'BlinkMacSystemFont',],
+      'serif': ['Georgia', 'Cambria', ],
+      'mono': ['SFMono-Regular', 'Menlo', ],
+      'display': ['Oswald', ],
+      'body': ['Open Sans', ],
+},
     extend: {
       colors: {
         'tomate': '#FF0012',
