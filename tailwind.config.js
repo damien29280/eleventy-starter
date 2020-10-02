c9ad27d081947e300859e52f517391316f0b2df6
damien <damien.villeneuve@edu.univ-fcomte.fr>
2020-10-02 15:19:24 +0200

    media query tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,28 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
-  purge: {
-    enabled: false,
-  },
-  theme: {
-    fontFamily: {
-      'sans': ['-apple-system', 'BlinkMacSystemFont',],
-      'serif': ['Georgia', 'Cambria', ],
-      'mono': ['SFMono-Regular', 'Menlo', ],
-      'display': ['Oswald', ],
-      'body': ['Open Sans', ],
-},
-    extend: {
-      colors: {
-        'tomate': '#FF0012',
-      }
+    future: {
+        removeDeprecatedGapUtilities: true,
+        purgeLayersByDefault: true,
     },
-  },
-  variants: {},
-  plugins: [],
+    purge: {
+        enabled: false,
+    },
+    theme: {
+        fontFamily: {
+            'sans': ['-apple-system', 'BlinkMacSystemFont',],
+            'serif': ['Georgia', 'Cambria',],
+            'mono': ['SFMono-Regular', 'Menlo',],
+            'display': ['Oswald',],
+            'body': ['Open Sans',],
+        },
+        extend: {
+            colors: {
+                'tomate': '#FF0012',
+                'dark': '#000000',
+            },
+            screens: {
+                'portrait': {'raw': '(orientation: portrait)'},            }
+        },
+    },
+    variants: {},
+    plugins: [],
 }
